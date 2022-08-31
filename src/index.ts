a5d2909472ba01bace8a937efdbb1b5cb12563a9
rsato1007 <robertmsato@gmail.com>
2022-08-30 23:22:47 -0600

    adding resources for learning React scheduler and fiber trees

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,31 @@
 /*
+    Documentation:
+    - A starting tutorial for recreating React: https://pomb.us/build-your-own-react/ 
+    - Good resource on reconcilation: https://reactjs.org/docs/reconciliation.html
+    - Deep dive into React Fiber resources: https://github.com/koba04/react-fiber-resources 
+    - Another resource on Fiber trees and Scheduler: https://philippspiess.com/scheduling-in-react/ 
+
+    React Concepts:
+    - Scheduler
+    - Fiber Trees
+    - Reconcilation
+    - Hooks
+    - Commits (updating and deleting nodes)
+
+    Additional Notes:
+    - Usage of workloop/scheduler for performing units of work in between each 
+    -- Workloop/scheduler allows the browser to perform various things in between each render.
+
     Current Task: Now for Reconilation, fiber trees, etc.
     --In terms of the next step, this might be a good place to start:
     https://www.velotio.com/engineering-blog/react-fiber-algorithm 
     -Let's review what the reconciler is.
 
-    Additional Notes:
-    - JSX: here's how the following is interpretted by JSX:
-    <div>
-        <p name="Robert Sato"> Hello World </p>
-    </div>
-
-    becomes:
-    Counter.createElement(
-        "div", 
-        null, 
-        Counter.createElement("p", {name: "Robert Sato"}, "Hello World") // This is our child element, but notice it's a recursive function call.
-    );
 */
 
 import { Counter } from "./Counter";
 import { CounterDOM } from "./CounterDOM";
 
-console.log(Counter.createElement("div", null));
+const elCounter = Counter.createElement("div", null, Counter.createElement("p", {name: "Robert Sato"}, "Hello World"));
 
-// const elCounter = Counter.createElement("div", null, Counter.createElement("p", {name: "Robert Sato"}, "Hello World"));
-
-// CounterDOM.render(elCounter, document.getElementById('app')!);
+CounterDOM.render(elCounter, document.getElementById('app')!);
